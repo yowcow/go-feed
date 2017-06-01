17f3fa4a4cc4611be6537923979bf7665dd9c710
yowcow <yowcow@cpan.org>
2017-06-01 21:21:34 +0900

    parallelism control

--- aggregator/aggregate.go
+++ aggregator/aggregate.go
@@ -2,49 +2,86 @@ package aggregator
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
-	"sync"
+	"time"
 )
 
 func Run(configFile, outputFile string) {
-	fmt.Println("Using config from", configFile)
+	fmt.Println("-- Using config from", configFile)
+
 	config := Config(configFile)
 
 	if len(outputFile) == 0 {
-		fmt.Println("Write output to STDOUT")
-		run(config, NewLogger(os.Stdout))
+		fmt.Println("-- Writing output to STDOUT")
+		run(config, NewLogging(os.Stdout))
 	} else {
-		fmt.Println("Write output to", outputFile)
+		fmt.Println("-- Writing output to", outputFile)
 		f, err := os.Create(outputFile)
 
 		if err != nil {
 			panic(fmt.Errorf("Failed opening output file to write", f, err))
 		}
 
-		run(config, NewLogger(f))
+		run(config, NewLogging(f))
 	}
 }
 
-func run(urls []string, logger *Log) {
-	wg := &sync.WaitGroup{}
+func worker(id int, logging *Logging, jobChan chan string, resChan chan int) {
+	for url := range jobChan {
+		fmt.Printf("Worker (%d): Start working on %s\n", id, url)
 
-	for _, url := range urls {
-		wg.Add(1)
-		go func(u string, w *sync.WaitGroup) {
-			fmt.Println("Begin working on", u)
-			rssXml, err := HttpGet(u)
-			if err == nil {
-				rssData := ParseRss(rssXml)
-				for _, item := range rssData.Items {
-					logger.Log(item.Link, item.Title)
-				}
-				fmt.Println("Done working on", u)
-			} else {
-				fmt.Println("Failed working on", u)
+		rssXml, err := HttpGet(url)
+
+		if err == nil {
+			fmt.Printf("Worker (%d): Successfully got response\n", id)
+
+			rssData := ParseRss(rssXml)
+
+			fmt.Printf("Worker (%d): Successfully parsed RSS\n", id)
+
+			count := 0
+
+			for _, item := range rssData.Items {
+				logging.Log(item.Link, item.Title)
+				count += 1
 			}
-			w.Done()
-		}(url, wg)
+
+			fmt.Printf("Worker (%d): Successfully logged %d items\n", id, count)
+
+			resChan <- count
+		} else {
+			fmt.Printf("Worker (%d): Failed getting response from %s (%s)\n", id, url, err)
+
+			resChan <- -1
+		}
+	}
+}
+
+func run(urls []string, logging *Logging) {
+	defer logging.Close()
+
+	jobChan := make(chan string, len(urls))
+	resChan := make(chan int, len(urls))
+
+	for w := 1; w <= 4; w++ {
+		go worker(w, logging, jobChan, resChan)
 	}
 
-	wg.Wait()
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range urls {
+		j := rand.Intn(i + 1)
+		urls[i], urls[j] = urls[j], urls[i]
+	}
+
+	for _, url := range urls {
+		jobChan <- url
+	}
+
+	close(jobChan)
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Printf("Main (%d): Logged item count %d\n", i, <-resChan)
+	}
 }
