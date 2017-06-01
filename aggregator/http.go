481446754de5b436f9a11cd4113fa3415c41efe3
yowcow <yowcow@cpan.org>
2017-06-01 21:20:50 +0900

    shut messages to stdout

--- aggregator/http.go
+++ aggregator/http.go
@@ -1,18 +1,14 @@
 package aggregator
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func HttpGet(url string) ([]byte, error) {
-	fmt.Println("Making GET request to", url)
-
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println("Request failed", url, err)
 		return nil, err
 	}
 
@@ -21,11 +17,8 @@ func HttpGet(url string) ([]byte, error) {
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		fmt.Println("Reading response body failed", url, err)
 		return nil, err
 	}
-
-	fmt.Println("Done making request to", url)
 
 	return body, nil
 }
