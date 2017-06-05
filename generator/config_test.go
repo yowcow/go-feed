4b8404901c65152f8951817745cf4ea50347aea3
yowcow <yowcow@cpan.org>
2017-06-05 18:04:00 +0900

    modify config_test

--- generator/config_test.go
+++ generator/config_test.go
@@ -5,13 +5,21 @@ import (
 	"testing"
 )
 
-func TestGetConfig(t *testing.T) {
+func TestConfig(t *testing.T) {
 	assert := assert.New(t)
 
-	config := Config("feed.json")
+	var config []*RssItem
+
+	config = Config("feed.json")
 
 	assert.Equal(3, len(config))
+
 	assert.Equal("ああ", config[0].Title)
+	assert.Equal("http://hoge", config[0].Link)
+
 	assert.Equal("いい", config[1].Title)
+	assert.Equal("http://fuga", config[1].Link)
+
 	assert.Equal("うう", config[2].Title)
+	assert.Equal("http://foo", config[2].Link)
 }
